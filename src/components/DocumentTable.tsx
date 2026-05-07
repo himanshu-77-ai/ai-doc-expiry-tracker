1c9fc5da7161e40546d754bcb0ba90a03219179e
himanshu-77-ai <himansh.cs91@gmail.com>
2026-05-07 23:27:07 +0100

    Update DocumentTable.tsx

--- src/components/DocumentTable.tsx
+++ src/components/DocumentTable.tsx
@@ -12,7 +12,7 @@ import {
   Edit,
   ExternalLink
 } from "lucide-react";
-import { format } from "date-fns";
+import { format, parseISO } from "date-fns";
 import { Document } from "../types";
 import { cn, getDynamicStatus } from "../lib/utils";
 
